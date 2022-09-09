caf4912fa61ccb218d0fee865d1c6655c8d5c599
Kelly, Chauncey <CKelly@WBMI.com>
2022-09-09 16:00:38 -0400

    All Components without state added to react to just prope

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Speakers from '../src/components/Speakers';
+import Speaker from '../src/components/Speaker';
+import { data } from '../SpeakerData';
+
+const IndexPage = () => {
+  return  <Speakers/>
+};
+
+export default IndexPage;
